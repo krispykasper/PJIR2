8cff553a4db7c2aa107c91b69fbe3a08223b70d2
krispykasper <krispy_kasnova@hotmail.com>
2018-10-03 21:49:35 +0700

    update

--- src/TFIDFSearcher.java
+++ src/TFIDFSearcher.java
@@ -3,6 +3,7 @@
 //ID: 
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -12,12 +13,44 @@ public class TFIDFSearcher extends Searcher
 		super(docFilename);
 		/************* YOUR CODE HERE ******************/
 
-		int df = 0, DocSize = documents.size();
-        Set<String> term = new HashSet<>();
+		int  docSize = documents.size();
+		double idf[], tf[][];
+        Set<String> allTerm = new HashSet<>();
 
 		for(Document document: documents){
-		    term.addAll(document.getTokens());
+		    allTerm.addAll(document.getTokens());
         }
+		idf = new double[allTerm.size()];
+		tf = new double[allTerm.size()][documents.size()];
+
+        String[] terms = new String[allTerm.size()];
+
+
+        Iterator<String> iterator = allTerm.iterator();
+
+
+        int a = 0;
+        while (iterator.hasNext()){
+            terms[a] = iterator.next() + "";
+            a++;
+        }
+
+        for(int i = 0; i < terms.length; i++){
+		    for(int j = 0; j < documents.size(); j++){
+		        if(documents.get(j).getTokens().contains(terms[i])){
+		            idf[i]++;
+                }
+            }
+
+            idf[i] = Math.log10(1 + ((double)docSize / idf[i]));
+            System.out.println(i);
+        }
+
+
+
+
+
+
 
 
 
