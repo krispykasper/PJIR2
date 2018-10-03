e1a917e4503321fa5608c62a3ecbc163a8260a46
krispykasper <krispy_kasnova@hotmail.com>
2018-10-03 19:22:27 +0700

    update

--- src/TFIDFSearcher.java
+++ src/TFIDFSearcher.java
@@ -11,7 +11,7 @@ public class TFIDFSearcher extends Searcher
 	public TFIDFSearcher(String docFilename) {
 		super(docFilename);
 		/************* YOUR CODE HERE ******************/
-		System.out.println("HIIIII");
+
 		Set<String> terms = new HashSet<>();
 		for (Document document: documents){
 			terms.addAll(document.getTokens());
@@ -26,32 +26,39 @@ public class TFIDFSearcher extends Searcher
 			}
 //			System.out.println("c"+count);
 			double idf = Math.log10((1+(documents.size()/count)));
+            int freq = 0;
+            double tf;
 			for(int j = 0; j < documents.size(); j++){
-				double tf;
-				int freq = 0;
+
 				if(documents.get(j).getTokens().contains(terms.toArray()[i])) {
 
+                    //System.out.println("doc "+documents.get(j).getTokens());
 					for(int l = 0; l < documents.get(j).getTokens().size();l++){
-						if(documents.get(j).getTokens().contains(terms.toArray()[i])){
-							freq++;
-						}
-					}
-//					System.out.println(freq);
+                        if(documents.get(j).getTokens().get(l).equals(terms.toArray()[i])){
+                            freq++;
+                        }
+                    }
 					tf = 1 + Math.log10(freq);
 					vsm[i][j] = idf*tf;
+
 				}
 				else {
 					tf = 0;
 					vsm[i][j] = idf*tf;
 				}
+                //System.out.println("tf"+tf);
+
 			}
 		}
 
-		for(int i = 0; i < vsm.length; i++){
-			for(int j = 0; j < vsm[0].length; j++){
-				System.out.print(vsm[i][j] + "\t");
-			}
-			System.out.println("\n");
+        System.out.println("VSM");
+            for(int i=0;i<vsm.length;i++){
+                for(int j=0;j<vsm[i].length;j++){
+                    System.out.println(vsm[i][j]);
+                }
+            }
+
+
 		}
 
 
@@ -60,7 +67,7 @@ public class TFIDFSearcher extends Searcher
 
 
 		/***********************************************/
-	}
+
 
 	@Override
 	public List<SearchResult> search(String queryString, int k) {
