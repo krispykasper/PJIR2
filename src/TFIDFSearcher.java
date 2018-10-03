f4898e54b3b10cd902436963495de34a4d208e5b
krispykasper <krispy_kasnova@hotmail.com>
2018-10-03 21:15:26 +0700

    update

--- src/TFIDFSearcher.java
+++ src/TFIDFSearcher.java
@@ -12,60 +12,18 @@ public class TFIDFSearcher extends Searcher
 		super(docFilename);
 		/************* YOUR CODE HERE ******************/
 
-		Set<String> terms = new HashSet<>();
-		for (Document document: documents){
-			terms.addAll(document.getTokens());
-		}
-		double[][] vsm = new double[terms.size()][documents.size()];
-		for(int i = 0; i < terms.size(); i++){
-			int count=0;
-			for(int k = 0; k < documents.size(); k++){
-				if(documents.get(k).getTokens().contains(terms.toArray()[i])) {
-					count++;
-				}
-			}
-//			System.out.println("c"+count);
-			double idf = Math.log10((1+(documents.size()/count)));
-            int freq = 0;
-            double tf;
-			for(int j = 0; j < documents.size(); j++){
+		int df = 0, DocSize = documents.size();
+        Set<String> term = new HashSet<>();
 
-				if(documents.get(j).getTokens().contains(terms.toArray()[i])) {
+		for(Document document: documents){
+		    term.addAll(document.getTokens());
+        }
 
-                    //System.out.println("doc "+documents.get(j).getTokens());
-					for(int l = 0; l < documents.get(j).getTokens().size();l++){
-                        if(documents.get(j).getTokens().get(l).equals(terms.toArray()[i])){
-                            freq++;
-                        }
-                    }
-					tf = 1 + Math.log10(freq);
-					vsm[i][j] = idf*tf;
 
-				}
-				else {
-					tf = 0;
-					vsm[i][j] = idf*tf;
-				}
-                //System.out.println("tf"+tf);
 
-			}
-		}
-
-        System.out.println("VSM");
-            for(int i=0;i<vsm.length;i++){
-                for(int j=0;j<vsm[i].length;j++){
-                    System.out.println(vsm[i][j]);
-                }
-            }
 
 
 		}
-
-
-
-
-
-
 		/***********************************************/
 
 
