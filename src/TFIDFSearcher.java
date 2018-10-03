9852caa5245b0cf4035315919541dee01cef505e
krispykasper <krispy_kasnova@hotmail.com>
2018-10-03 23:54:38 +0700

    update

--- src/TFIDFSearcher.java
+++ src/TFIDFSearcher.java
@@ -2,65 +2,105 @@
 //Section: 
 //ID: 
 
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-public class TFIDFSearcher extends Searcher
-{
-	public TFIDFSearcher(String docFilename) {
-		super(docFilename);
-		/************* YOUR CODE HERE ******************/
+public class TFIDFSearcher extends Searcher {
 
-		int  docSize = documents.size();
-		double idf[], tf[][];
-        Set<String> allTerm = new HashSet<>();
+    private Set<String> terms;
+    private double[][] vsm;
+    private double[] idf;
 
-		for(Document document: documents){
-		    allTerm.addAll(document.getTokens());
+    public TFIDFSearcher(String docFilename) {
+        super(docFilename);
+        /************* YOUR CODE HERE ******************/
+
+        terms = new HashSet<>();
+        for (Document document : documents) {
+            terms.addAll(document.getTokens());
         }
-		idf = new double[allTerm.size()];
-		tf = new double[allTerm.size()][documents.size()];
-
-        String[] terms = new String[allTerm.size()];
-
-        Iterator<String> iterator = allTerm.iterator();
-
-        int a = 0;
-        while (iterator.hasNext()){
-            terms[a] = iterator.next() + "";
-            a++;
-        }
-        int frequency = 0;
-
-        for(int i = 0; i < terms.length; i++){
-		    for(int j = 0; j < documents.size(); j++){
-		        for(int k = 0; k < documents.get(j).getTokens().size(); k++){
-		            if(terms[i].equals(documents.get(j).getTokens().get(k))){
-		                frequency++;
-                    }
+        vsm = new double[terms.size()][documents.size()];
+        for (int i = 0; i < terms.size(); i++) {
+            int count = 0;
+            //System.out.println(terms.toArray()[i]);
+            for (int k = 0; k < documents.size(); k++) {
+                if (documents.get(k).getTokens().contains(terms.toArray()[i])) {
+                    count++;
                 }
+            }
+//			System.out.println("c"+count);
+            idf = new double[terms.size()];
+            idf[i] = Math.log10((1 + ((double) documents.size() / count)));
+            int freq = 0;
+            double tf;
+            for (int j = 0; j < documents.size(); j++) {
 
-		        if(documents.get(j).getTokens().contains(terms[i])){
-		            idf[i]++;
+                if (documents.get(j).getTokens().contains(terms.toArray()[i])) {
+
+                    //System.out.println("doc " + documents.get(j).getTokens());
+                    for (int l = 0; l < documents.get(j).getTokens().size(); l++) {
+                        if (documents.get(j).getTokens().get(l).equals(terms.toArray()[i])) {
+                            freq++;
+                        }
+                    }
+                    tf = 1 + Math.log10(freq);
+                    vsm[i][j] = idf[i] * tf;
+
+                } else {
+                    vsm[i][j] = 0;
                 }
 
             }
-            idf[i] = Math.log10(1 + ((double)docSize / idf[i]));
-            System.out.println(i);
+        }
+
+    }
+
+
+    /***********************************************/
+
+
+    @Override
+    public List<SearchResult> search(String queryString, int k) {
+        /************* YOUR CODE HERE ******************/
+        List<String> qTokens = tokenize(queryString);
+        double[] q = new double[terms.size()];
+        int freq = 0;
+
+        for (int i = 0; i < terms.size(); i++) {
+            if (!qTokens.contains(terms.toArray()[i])) {
+
+                q[i] = 0;
+
+                continue;
+            }
+            for (int j = 0; j < qTokens.size(); j++) {
+                if (qTokens.get(j).equals(terms.toArray()[i])) {
+
+                    freq++;
+                }
+            }
+            q[i] = (1 + Math.log10(freq)) * idf[i];
+
         }
 
 
-		}
-		/***********************************************/
+        List<SearchResult> resultList = new ArrayList<>();
+        double sum = 0, qSum = 0, dSum = 0;
+        for (int j = 0; j < vsm.length; j++) {
+            for (int i = 0; i < vsm[0].length; i++) {
+                sum += (q[i] * vsm[i][j]);
+                qSum += Math.pow(q[i],2);
+                dSum += Math.pow(vsm[i][j],2);
+            }
+
+            double cosine = ((sum)/(Math.sqrt(qSum))*Math.sqrt(dSum));
+            resultList.add(new SearchResult(documents.get(j), cosine));
+        }
 
 
-	@Override
-	public List<SearchResult> search(String queryString, int k) {
-		/************* YOUR CODE HERE ******************/
-
-		return null;
-		/***********************************************/
-	}
+        return resultList;
+        /***********************************************/
+    }
 }
