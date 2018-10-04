7391bc49d98bb46e924dbcf81b2fd7462e947b33
krispykasper <krispy_kasnova@hotmail.com>
2018-10-04 07:12:00 +0700

    update

--- src/TFIDFSearcher.java
+++ src/TFIDFSearcher.java
@@ -22,26 +22,28 @@ public class TFIDFSearcher extends Searcher {
             terms.addAll(document.getTokens());
         }
         vsm = new double[terms.size()][documents.size()];
+        String[] termArray = terms.toArray(new String[0]);
+
         for (int i = 0; i < terms.size(); i++) {
             int count = 0;
             //System.out.println(terms.toArray()[i]);
             for (int k = 0; k < documents.size(); k++) {
-                if (documents.get(k).getTokens().contains(terms.toArray()[i])) {
+                List<String> list = documents.get(k).getTokens();
+                if (list.contains(termArray[i])) {
                     count++;
                 }
             }
-//			System.out.println("c"+count);
             idf = new double[terms.size()];
             idf[i] = Math.log10((1 + ((double) documents.size() / count)));
             int freq = 0;
             double tf;
             for (int j = 0; j < documents.size(); j++) {
-
-                if (documents.get(j).getTokens().contains(terms.toArray()[i])) {
+                List<String> list = documents.get(j).getTokens();
+                if (list.contains(termArray[i])) {
 
                     //System.out.println("doc " + documents.get(j).getTokens());
-                    for (int l = 0; l < documents.get(j).getTokens().size(); l++) {
-                        if (documents.get(j).getTokens().get(l).equals(terms.toArray()[i])) {
+                    for (int l = 0; l < list.size(); l++) {
+                        if (list.get(l).equals(termArray[i])) {
                             freq++;
                         }
                     }
@@ -52,6 +54,9 @@ public class TFIDFSearcher extends Searcher {
                     vsm[i][j] = 0;
                 }
 
+            }
+            if(i % 100 == 0){
+                System.out.println(i);
             }
         }
 
@@ -67,16 +72,17 @@ public class TFIDFSearcher extends Searcher {
         List<String> qTokens = tokenize(queryString);
         double[] q = new double[terms.size()];
         int freq = 0;
+        String[] termArray = terms.toArray(new String[0]);
 
         for (int i = 0; i < terms.size(); i++) {
-            if (!qTokens.contains(terms.toArray()[i])) {
+            if (!qTokens.contains(termArray[i])) {
 
                 q[i] = 0;
 
                 continue;
             }
             for (int j = 0; j < qTokens.size(); j++) {
-                if (qTokens.get(j).equals(terms.toArray()[i])) {
+                if (qTokens.get(j).equals(termArray[i])) {
 
                     freq++;
                 }
@@ -88,15 +94,18 @@ public class TFIDFSearcher extends Searcher {
 
         List<SearchResult> resultList = new ArrayList<>();
         double sum = 0, qSum = 0, dSum = 0;
-        for (int j = 0; j < vsm.length; j++) {
-            for (int i = 0; i < vsm[0].length; i++) {
+        for (int j = 0; j < vsm[0].length; j++) {
+            for (int i = 0; i < vsm.length; i++) {
                 sum += (q[i] * vsm[i][j]);
                 qSum += Math.pow(q[i],2);
                 dSum += Math.pow(vsm[i][j],2);
             }
 
             double cosine = ((sum)/(Math.sqrt(qSum))*Math.sqrt(dSum));
-            resultList.add(new SearchResult(documents.get(j), cosine));
+            if(!Double.isNaN(cosine)){
+                resultList.add(new SearchResult(documents.get(j), cosine));
+
+            }
         }
 
 
