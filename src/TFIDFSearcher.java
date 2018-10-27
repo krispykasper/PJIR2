3fe134122309cac0ecd0ce49e37c5faf79b90aea
krispykasper <krispy_kasnova@hotmail.com>
2018-10-27 18:25:12 +0700

    update poch

--- src/TFIDFSearcher.java
+++ src/TFIDFSearcher.java
@@ -6,26 +6,31 @@
 //Name: Manisa Satravisut
 //ID: 5988209
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class TFIDFSearcher extends Searcher
 {
     private HashMap<String, Double> idf = new HashMap<>();
     private HashMap<String, HashMap<Integer, Double>> wd = new HashMap<>();
+    private HashSet<String> terms = new HashSet<>();
 
 
     public TFIDFSearcher(String docFilename) {
         super(docFilename);
         /************* YOUR CODE HERE ******************/
         HashMap<String, HashMap<Integer, Integer>> freq = new HashMap<>();
-        HashSet<String> terms = new HashSet<>();
-        HashMap<Integer, List<String>> documentToken = new HashMap<>();
+
         for(Document document: documents){
+            //add all document's tokens to terms HashSet
             terms.addAll(document.getTokens());
             List<String> token = document.getTokens();
-            documentToken.put(document.getId(), token);
+            //Set number of
             for(String term: token){
                 if(freq.containsKey(term)){
+
                     if(freq.get(term).containsKey(document.getId())){
                         freq.get(term).put(document.getId(), freq.get(term).get(document.getId()) + 1);
                     }else {
@@ -35,6 +40,7 @@ public class TFIDFSearcher extends Searcher
                     freq.put(term, new HashMap<>());
                     freq.get(term).put(document.getId(), 1);
                 }
+
             }
         }
         for(String term: terms){
@@ -47,12 +53,24 @@ public class TFIDFSearcher extends Searcher
             idf.put(term, Math.log10(1 + (double) documents.size() / count));
             for (Document document: documents){
                 if(freq.get(term).get(document.getId()) != null){
-                    wd.put(term, new HashMap<>());
-                    wd.get(term).put(document.getId(), idf.get(term) * (1d + Math.log10(freq.get(term).get(document.getId()))));
+                    if(wd.containsKey(term)){
+
+                        wd.get(term).put(document.getId(), idf.get(term) * (1d + Math.log10(freq.get(term).get(document.getId()))));
+
+                    }else {
+
+                        wd.put(term, new HashMap<>());
+                        wd.get(term).put(document.getId(), idf.get(term) * (1d + Math.log10(freq.get(term).get(document.getId()))));
+
+                    }
                 }
+
+
             }
         }
         System.out.println("First Part");
+
+
 
 
         /***********************************************/
@@ -62,9 +80,80 @@ public class TFIDFSearcher extends Searcher
     public List<SearchResult> search(String queryString, int k) {
         /************* YOUR CODE HERE ******************/
 
+        HashMap<String, Integer> freq = new HashMap<>();
+        List<String> q = tokenize(queryString);
+        HashMap<String, Double> wq = new HashMap<>();
+        List<SearchResult> resultList = new ArrayList<>();
+        List<SearchResult> choosedResultList = new ArrayList<>();
+
+        for(String term: q){
+            if(freq.containsKey(term)){
+                freq.put(term, freq.get(term) + 1);
+                System.out.println(freq.get(term));
+            }else {
+                freq.put(term, 1);
+            }
+        }
+
+        for(String term: terms){
+            if(freq.get(term) != null){
+                wq.put(term, idf.get(term) * (1d + Math.log10(freq.get(term))));
+            }
+        }
+
+        for(Document document: documents){
+            double sumAB = 0;
+            double sumA = 0;
+            double sumB = 0;
+            for(String term: terms){
+                double a, b;
+                if(wq.get(term) == null){
+                    a = 0;
+                }else {
+                    a = wq.get(term);
+                }
+
+                if(wd.get(term).get(document.getId()) == null){
+                    b = 0;
+                }else {
+                    b = wd.get(term).get(document.getId());
+                }
+                sumAB += a * b;
+                sumA += a * a;
+                sumB += b * b;
+            }
+            resultList.add(new SearchResult(document, sumAB / ((Math.sqrt(sumA)) * (Math.sqrt(sumB)))));
+        }
+        System.out.println("Second Part");
+
+        for (int i = 0; i < k; i++) {
+            int ind = -1;
+            SearchResult temp = resultList.get(i);
+            for (int j = i + 1; j < resultList.size(); j++) {
+                if (resultList.get(j).getScore() > temp.getScore()) {
+                    temp = resultList.get(j);
+                    ind = j;
+                }
+            }
+
+            if (temp.getDocument().getId() != resultList.get(i).getDocument().getId()) {
+                resultList.set(ind, resultList.get(i));
+                resultList.set(i, temp);
+            }
+
+            choosedResultList.add(resultList.get(i));
+        }
+        System.out.println("Third Part");
 
 
-        return new ArrayList<>();
+
+
+
+        return choosedResultList;
+
+
+
+
         /***********************************************/
     }
 }
